3323e83c075bb5ebd728e145da0be21dc34ebf95
Heiman Chan <heimanchan0@gmail.com>
2019-04-14 00:29:16 -0700

    Solved leetcode 518 change2

--- memoization/memoization_project/lib/leet_code_518.js
+++ memoization/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,18 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+let change = function (amount, coins, memo = {}) {
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  let currCoin = coins[coins.length - 1];
+  let count = 0;
+
+  for (let i = 0; currCoin * i <= amount; i++) {
+    count += change(amount - currCoin * i, coins.slice(0,-1), memo);
+  }
+
+  memo[key] = count;
+  return memo[key]
+};
--- memoization/memoization_project/lib/problems.js
+++ memoization/memoization_project/lib/problems.js
@@ -40,7 +40,7 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
-  if (amount in memo) return memo[amount];
+  if (memo[amount]) return memo[amount];
   if (amount === 0) return 0;
    
   let count = [];
