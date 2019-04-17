75312e01674e89cdb17c671a2560ab0c9046e18a
Heiman Chan <heimanchan0@gmail.com>
2019-04-16 23:53:00 -0700

    Solved leetcode no.17

---
+++ leetcode/17_Letter_Combinations_of_a_Phone_Number.js
@@ -0,0 +1,333 @@
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx+1);
+      word.pop();
+    }
+  }
+}
+
+function letterCombinations(digits) {
+  let map = {
+    0: " ",
+    1: "",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  }
+
+  let result = [];
+  let word = [];
+
+  if (digits.length) {
+    dfs(0);
+  } else {
+    return [];
+  }
+
+  return result;
+
+  function dfs(idx) {
+    if (idx === digits.length) result.push(word.join(""));
+
+    let letters = map[digits[idx] - '0'];
+    for (let i = 0; i < letters.length; i++) {
+      word.push(letters[i]);
+      dfs(idx + 1);
+      word.pop();
+    }
+  }
+}
+
