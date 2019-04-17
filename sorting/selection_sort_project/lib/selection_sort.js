9192d15c67336b9e6f7ba8deb66c34c2409b76f7
Heiman Chan <heimanchan0@gmail.com>
2019-04-16 17:07:47 -0700

    Implement selection sort

---
+++ sorting/selection_sort_project/lib/selection_sort.js
@@ -0,0 +1,23 @@
+function swap(arr, index1, index2) {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+  return arr
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIdx = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIdx] > arr[j]) minIdx = j;
+    }
+
+    swap(arr, i, minIdx)
+  }
+
+  return arr;
+}
+
+module.exports = {
+    selectionSort,
+    swap
+};
