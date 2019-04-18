1fba5d65241726747b68129501705494390ad29a
Heiman Chan <heimanchan0@gmail.com>
2019-04-17 22:43:26 -0700

    Implement counting sort

---
+++ sorting/counting_sort_project/lib/counting_sort.js
@@ -0,0 +1,22 @@
+function countingSort(arr, max) {
+  let result = [];
+  let counters = new Array(max + 1).fill(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    counters[arr[i]]++;
+  }
+
+  for (let j = 0; j < counters.length; j++) {
+    while(counters[j] > 0) {
+      result.push(j);
+      counters[j]--;
+    }
+  }
+
+  return result;
+}
+
+
+module.exports = {
+    countingSort
+};
