420eb9680bc1b20e82cbb6d29972b4ff81a21192
Heiman Chan <heimanchan0@gmail.com>
2019-04-17 21:46:46 -0700

    Implement quick sort

---
+++ sorting/quick_sort_project/lib/quick_sort.js
@@ -0,0 +1,14 @@
+function quickSort(array) {
+  if (array.length <= 1) return array;
+
+  let pivot = array.shift();
+  let left = quickSort(array.filter(el => el < pivot));
+  let right = quickSort(array.filter(el => el >= pivot));
+
+  return left.concat([pivot], right);
+}
+
+
+module.exports = {
+    quickSort
+};
