3951675d7b644f8390aefdd441163eabf1717bae
Heiman Chan <heimanchan0@gmail.com>
2019-04-23 12:16:15 -0700

    Implemenet BST

--- trees/bst_project/lib/leet_code_110.js
+++ trees/bst_project/lib/leet_code_110.js
@@ -3,5 +3,14 @@
 
 
 function isBalanced(root) {
+  if (!root) return -1;
+  let leftHeight = treeHeight(root.left)
+  let rightHeight = treeHeight(root.right)
 
+  return Math.abs(leftHeight - rightHeight) <= 1
+}
+
+function treeHeight(root) {
+  if (!root) return -1;
+  return 1 + Math.max(treeHeight(root.left), treeHeight(root.right))
 }
