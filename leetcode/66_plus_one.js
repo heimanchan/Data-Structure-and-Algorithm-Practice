9beeff24a87d1478eb7a5b41bb5049f6ac5c7a68
Heiman Chan <heimanchan0@gmail.com>
2019-04-23 17:32:33 -0700

    Solved leetcode 66. plus one

---
+++ leetcode/66_plus_one.js
@@ -0,0 +1,18 @@
+const plusOne = function (digits) {
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+  
+  digits.unshift(1);
+  
+  return digits;
+};
+
+console.log(plusOne([9]))
+console.log(plusOne([1,2,3]))
