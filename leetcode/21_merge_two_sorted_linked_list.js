dbc0e51a1893a15603952a2bf9c9abecc9d4ee7a
Heiman Chan <heimanchan0@gmail.com>
2019-04-29 18:28:23 -0700

    Solve 21. merge two sorted linked list

---
+++ leetcode/21_merge_two_sorted_linked_list.js
@@ -0,0 +1,24 @@
+const mergeTwoLists = function(l1, l2) {
+  let dummy = new ListNode(-1);
+  let curr = dummy;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = new ListNode(l1.val);
+      curr = curr.next;
+      l1 = l1.next;
+    } else {
+      curr.next = new ListNode(l2.val);
+      curr = curr.next;
+      l2 = l2.next;
+    }
+  }
+  
+  if (l1) {
+    curr.next = l1;
+  } else {
+    curr.next = l2;
+  }
+  
+  return dummy.next
+};
