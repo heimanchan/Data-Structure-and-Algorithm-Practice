fb81971d79027b06273fa2f63ce43481e912d5e9
Heiman Chan <heimanchan0@gmail.com>
2019-05-13 17:37:45 -0700

    Solve string rotation

--- cracking_the_coding_interview/arrays_string/stringRotation.js
+++ cracking_the_coding_interview/arrays_string/stringRotation.js
@@ -1,3 +1,17 @@
-const stringRotation = (s1, s2) => {
-  
+const isRotation = (s1, s2) => {
+  let len = s1.length;
+
+  if (len > 0 && len === s2.length) {
+    let s1s1 = s1 + s1;
+    console.log(s1s1)
+    return isSubString(s1s1, s2);
+  }
+
+  return false;
 }
+
+const isSubString = (s1, s2) => {
+  return s1.indexOf(s2) > -1;
+}
+
+console.log(isRotation("waterbottle", "erbottlewat"));
