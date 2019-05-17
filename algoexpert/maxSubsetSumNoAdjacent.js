2bbc24f36600400864cf3ac9104457776a94cdad
Heiman Chan <heimanchan0@gmail.com>
2019-05-17 14:49:43 -0700

    Solve maxSubsetSumNoAdjacent

---
+++ algoexpert/maxSubsetSumNoAdjacent.js
@@ -0,0 +1,18 @@
+function maxSubsetSumNoAdjacent(array) {
+	if (array.length < 1) return 0;
+	if (array.length === 1) return array[0];
+	let second = array[0]
+	let first = Math.max(array[1], array[0]);
+	
+	for (let i = 2; i < array.length; i++) {
+		let currMax = Math.max(first, second+array[i]);
+													 
+		second = first;
+		first = currMax;
+	}
+	
+	return first;
+}
+
+// Do not edit the line below.
+exports.maxSubsetSumNoAdjacent = maxSubsetSumNoAdjacent;
