5a30d25cbf1b498851341e7ebe79b4fe6a459283
Heiman Chan <heimanchan0@gmail.com>
2019-05-22 00:54:05 -0700

    Solve 414. Third Maximum Number

---
+++ leetcode/414.thirdMax.js
@@ -0,0 +1,19 @@
+const thirdMax = function(nums) {
+  if (nums.length < 3) return Math.max(...nums)
+  let first = second = third = -Infinity;
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === first || nums[i] === second || nums[i] === third) continue;
+    if (nums[i] > first) {
+      third = second;
+      second = first;
+      first = nums[i];
+    } else if (first > nums[i] && nums[i] > second) {
+      third = second;
+      second = nums[i]
+    } else if (second > nums[i] && nums[i] > third) {
+      third = nums[i]
+    }
+  }
+  return third !== -Infinity ? third : first;
+};
