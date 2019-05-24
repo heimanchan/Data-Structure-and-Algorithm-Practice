2c368d31c11e18774af568f67d4aa4e246b2722b
Heiman Chan <heimanchan0@gmail.com>
2019-05-23 21:38:19 -0700

    Solve rremoveKthNodeFromEnd by using two pointers

--- algoexpert/removeKthNodeFromEnd.js
+++ algoexpert/removeKthNodeFromEnd.js
@@ -1,24 +1,41 @@
 function removeKthNodeFromEnd(head, k) {
-	let count = 0;
-	let node = head;
-	while(node) {
-		count++;
-		node = node.next;
-	}
+	// let count = 0;
+	// let node = head;
+	// while(node) {
+	// 	count++;
+	// 	node = node.next;
+	// }
 
-	if (k === count) {
-		head.value = head.next.value
-		head.next = head.next.next;
-	} else {
-		node = head;
-		target = count - k;
-		count = 0;
-		while(node) {
-			if (++count === target) {
-				node.next = node.next.next;
-			} else {
-				node = node.next;
-			}
-		}
-	}
+	// if (k === count) {
+	// 	head.value = head.next.value
+	// 	head.next = head.next.next;
+	// } else {
+	// 	node = head;
+	// 	target = count - k;
+	// 	count = 0;
+	// 	while(node) {
+	// 		if (++count === target) {
+	// 			node.next = node.next.next;
+	// 		} else {
+	// 			node = node.next;
+	// 		}
+	// 	}
+  // }
+  let count = 0;
+  let first = second = head;
+  while (count++ < k) {
+    second = second.next;
+  }
+
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+  }
+
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+
+  first.next = first.next.next;
 }
