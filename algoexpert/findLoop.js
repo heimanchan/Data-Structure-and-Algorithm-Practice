b7f0f5ec7deed0dfe9b5809eb852ff42b717da03
Heiman Chan <heimanchan0@gmail.com>
2019-05-29 16:15:44 -0700

    Refactor findLoop

--- algoexpert/findLoop.js
+++ algoexpert/findLoop.js
@@ -1,17 +1,16 @@
 function findLoop(head) {
-	let slow = head.next;
-	let fast = head.next.next;
-	while (slow !== fast) {
-		slow = slow.next;
-		fast = fast.next.next;
-		if (slow === fast) {
-			slow = head;
-			while (slow !== fast) {
-				slow = slow.next;
-				fast = fast.next;
-			}
-		}
-	}
-	
-	return slow
+  let slow = head.next;
+  let fast = head.next.next;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  slow = head;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow
 }
