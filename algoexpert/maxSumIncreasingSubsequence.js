ed25c7fe26c68842ba50c906374b610de7200c8d
Heiman Chan <heimanchan0@gmail.com>
2019-06-19 21:29:45 -0700

    Refactor maxSumIncreasingSubsequence

--- algoexpert/maxSumIncreasingSubsequence.js
+++ algoexpert/maxSumIncreasingSubsequence.js
@@ -1,31 +1,29 @@
 function maxSumIncreasingSubsequence(array) {
-	let sums = [...array];
-	let sub = new Array(array.length);
-	let maxSumIdx = 0;
-	sums[0] = array[0];
-	
-	for (let i = 1; i < array.length; i++) {
-		let curr = array[i];
-		let currSum = curr;
-		for (let j = 0; j < i; j++) {
-			if (array[j] < curr && sums[j] + curr >= sums[i]) {
-				sums[i] = sums[j] + curr;
-				sub[i] = j;
-			}
-		}
-		
-		if (sums[i] >= sums[maxSumIdx]) maxSumIdx = i;
-	}
-	
-	return [sums[maxSumIdx], makeSub(maxSumIdx)]
-	function makeSub(idx) {
-		let seq = [];
-		
-		while (array[idx]) {
-			seq.unshift(array[idx]);
-			idx = sub[idx];
-		}
-		
-		return seq;
-	}
+  let sums = [...array]
+  let subs = new Array(array.length);
+  let maxIdx = 0;
+
+  for (let i = 1; i < array.length; i++) {
+    let curr = array[i];
+    for (let j = 0; j < i; j++) {
+      if (array[j] < curr && curr + sums[j] >= sums[i]) {
+        sums[i] = curr + sums[j]
+        subs[i] = j
+      }
+    }
+
+    if (sums[i] >= sums[maxIdx]) maxIdx = i
+  }
+
+  return [sums[maxIdx], makeIncreasingSeq(maxIdx)]
+
+  function makeIncreasingSeq(idxToTrace) {
+    let seq = [];
+    while (array[idxToTrace]) {
+      seq.unshift(array[idxToTrace])
+      idxToTrace = subs[idxToTrace]
+    }
+
+    return seq;
+  }
 }
