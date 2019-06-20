86eb6e7c231181de7ce5abe364dadce2cabaabd6
Heiman Chan <heimanchan0@gmail.com>
2019-06-19 23:45:01 -0700

    Refactor 1051. Height Checker to O(n) solution

--- leetcode/1051_heightChecker.js
+++ leetcode/1051_heightChecker.js
@@ -1,10 +1,27 @@
 const heightChecker = function(heights) {
-  const sorted = Array.from(heights).sort((a, b) => (a-b))
-  let result = 0;
-  
-  for (let i = 0; i < heights.length; i++) {
-    if (sorted[i] !== heights[i]) result++;
+  // Time: O(n)
+  const numsArray = new Array(101).fill(0);
+  let heightsIdx = result = 0;
+
+  for (const height of heights) {
+    numsArray[height]++;
   }
+
+  for (let i = 1; i < numsArray.length; i++) {
+    let numOccurrence = numsArray[i]
+    for (let j = 0; j < numOccurrence; j++) {
+      if (heights[heightsIdx++] !== i) result++;
+    }
+  }
+
+  return result
+  // Time: O(nlogn)
+  // const sorted = Array.from(heights).sort((a, b) => (a-b))
+  // let result = 0;
   
-  return result;
+  // for (let i = 0; i < heights.length; i++) {
+  //   if (sorted[i] !== heights[i]) result++;
+  // }
+  
+  // return result;
 };
