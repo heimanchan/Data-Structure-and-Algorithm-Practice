cb4563483a681caf6f714813ee8c30cb17e07227
Heiman Chan <heimanchan0@gmail.com>
2019-06-24 22:34:57 -0700

    Redo validateBST

--- algoexpert/validateBST.js
+++ algoexpert/validateBST.js
@@ -1,6 +1,13 @@
-function validateBst(tree, min = -Infinity, max = Infinity) {
-	if (!tree) return true;
-	if (tree.value >= max || tree.value < min) return false;
-		
-	return validateBst(tree.left, min, tree.value) && validateBst(tree.right, tree.value, max)
+// Validate if a BST is valid
+
+function validateBst(tree) {
+  return validateBSTHelper(tree, -Infinity, Infinity);
+}
+
+function validateBSTHelper(tree, min, max) {
+  if (!tree) return true;
+  if (tree.value >= max || tree.value < min) return false;
+
+  return validateBSTHelper(tree.left, min, tree.value) &&
+    validateBSTHelper(tree.right, tree.value, max)
 }
