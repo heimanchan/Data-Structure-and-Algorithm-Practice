673d485e80702e8fc68a069316d7dbe3038628ee
Heiman Chan <heimanchan0@gmail.com>
2019-07-02 13:01:10 -0700

    Implement Score class

--- leaderboard.js
+++ leaderboard.js
@@ -1,36 +1,140 @@
+/*
+Exercise Goal:
+    - The goal of this exercise is to show us how you apply software engineering 
+    principles to create a maintainable software solution.
+
+    How to approach this:
+
+            - Don't worry about persistence. It would make sense here, but for this
+            exercise only use in-memory data structures.
+            - Don't worry about tricks or "gotchyas", as there aren't any.
+            - Just focus on writing clean maintainable code.
+
+
+
+Specification:
+
+    Create a class LeaderBoard whose interface includes the following methods:
+
+    Method Name: add_score
+
+        - Add a new score to the player's average. If a player doesn't exist in the 
+        LeaderBoard, they will be automatically added.
+
+        Args:
+
+                player_id (Integer): The player's ID.
+                score (Integer): The score to record for the player
+
+        Returns:
+
+                Double: The new average score for the given player
+
+    Method Name: top
+
+        - Get the top player_ids on the leaderboard ordered by their average scores
+        from highest to lowest
+
+        Args:
+
+                num_players (Integer): The maximum number of player_ids to return
+
+        Returns:
+
+                List<Integer>: a list of player_ids
+
+    Method Name: reset
+
+        - Removes any scoring information for a player, effectively 
+        resetting them to 0
+
+        Args:
+
+                player_id (Integer): The player's ID.
+
+Example Usage:
+
+
+    // Create a new LeaderBoard Instance
+    LeaderBoard leader_board = new LeaderBoard();
+
+    // Add scores for players to the LeaderBoard
+    leader_board.add_score(1, 50); // 50.0
+    leader_board.add_score(2, 80); // 80.0
+    leader_board.add_score(2, 70); // 75.0
+    leader_board.add_score(2, 60); // 70.0
+    leader_board.add_score(3, 90); // 90.0
+    leader_board.add_score(3, 85); // 87.5
+
+    // Get top positions for the leaderboard
+    leader_board.top(3); // [3, 2, 1]
+    leader_board.top(2); // [3, 2]
+    leader_board.top(1); // [3]
+
+    // Reset a player 3's scores
+    leader_board.reset(3); // void
+
+    // Player 3 is now at the bottom of the leaderboard
+    leader_board.top(3); // [2, 1, 3]
+
+Expected values
+
+    - Player IDs will always be positive integers small enough to be 
+    stored as a signed 32-bit integer Scores are integers ranging from 0-100
+
+
+We have provided stubbed out code and tests for you below. Please note that these tests are not exhaustive and do not cover all corner cases. We recommend extending the given tests to ensure your code is correct.
+
+*/
+
+
+// Your code goes here. Feel free to make helper classes if needed
 class LeaderBoard {
   constructor() {
     this.board = [];
     this.playersMap = {};
     this.scoresIdxMap = {};
   }
-  
-  add_score = (player_id, score) => {
+
+  add_score(player_id, score) {
     // playersMap stores playerID as key and a player object as value
-    
+
     if (!this.playersMap.hasOwnProperty(player_id)) {
       this.playersMap[player_id] = new Player(player_id);
-    } 
-    
+    }
+
     const currPlayer = this.playersMap[player_id];
     currPlayer.addScore(score);
     this.addScoreToBoard(currPlayer.id);
-    
+
     return currPlayer.average;
   };
-  
 
-  top = (num_players) => {
+
+  top(num_players) {
     const tops = [];
-    
+
     for (let i = 0; i < num_players; i++) {
       tops.push(this.board[i][0]);
     }
-    
+
     return tops;
   };
-  
-  reset = (player_id) => {
+
+  bottom(num_players) {
+    // 4, 3    num_players = 3
+    const bottom = [];
+
+    for (let i = this.board.length - 1; i >= 0; i--) {
+      if (bottom.length === num_players) break;
+
+      bottom.push(this.board[i][0]);
+    }
+
+    return bottom;
+  }
+
+  reset(player_id) {
     this.playersMap[player_id].resetScores();
     this.addScoreToBoard(player_id);
   };
@@ -38,7 +142,7 @@ class LeaderBoard {
   addScoreToBoard(playerID) {
     let currPlayer = this.playersMap[playerID];
     let average = currPlayer.average;
-    
+
     // first record
     if (!this.board.length) {
       this.board.push([playerID, average]);
@@ -48,16 +152,16 @@ class LeaderBoard {
       if (this.scoresIdxMap.hasOwnProperty(playerID)) {
         this.board.splice(this.scoresIdxMap, 1);
       }
-      
+
       for (let i = 0; i < this.board.length; i++) {
         if (average >= this.board[i][1]) {
           this.board.splice(i, 0, [playerID, average]);
           this.scoresIdxMap[playerID] = i;
-          
+
           return;
         }
       }
-      
+
       // Add the record at the end if its score is smallest
       this.board.push([playerID, average]);
     }
@@ -71,23 +175,47 @@ class Player {
     this.scores = [];
     this.average;
   }
-  
+
   addScore(score) {
-    this.scores.push(score);
+    const newScore = new Score(score);
+    this.scores.push(newScore);
     this.averageScore();
   }
-  
+
   averageScore() {
     if (!this.scores.length) return 0;
-    
-    let average = this.scores.reduce((acc, score) => acc + score)
-    this.average = average / this.scores.length
+
+    // let average = this.scores.reduce((acc, scoreObj) => acc + scoreObj.score);
+    let average = 0;
+
+    for (let i = 0; i < this.scores.length; i++) {
+      if (!this.scores[i].isExpired()) average += this.scores[i].score;
+    }
+
+    this.average = average / this.scores.length;
   }
-  
+
   resetScores() {
     this.average = 0;
   }
 }
+
+class Score {
+  constructor(score) {
+    this.score = score;
+    this.date = new Date();
+  }
+
+  isExpired() {
+    const now = new Date();
+    now.getDate();
+    this.date.getDate();
+
+    const sevenDaysMilliSec = 24 * 60 * 60 * 1000 * 7;
+    return now - this.date > sevenDaysMilliSec;
+  }
+}
+
 
 // Test code here
 
@@ -123,4 +251,7 @@ console.log(array_equals(leader_board.top(4), [3, 2, 1, 4]))
 leader_board.reset(3)
 console.log('After reset top 4 [' + leader_board.top(4) + '] should equal [2, 1, 4, 3]')
 console.log(array_equals(leader_board.top(4), [2, 1, 4, 3]))
+
+console.log("Bottom 3")
+console.log(array_equals(leader_board.bottom(3), [3, 4, 1]))
 
