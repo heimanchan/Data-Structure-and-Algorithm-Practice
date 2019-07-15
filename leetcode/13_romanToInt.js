923d9de993f9f189e9fe9e33a4b3409cbc3e54cf
Heiman Chan <heimanchan0@gmail.com>
2019-07-15 16:30:30 -0700

    Redo 13. Roman to Integer

--- leetcode/13_romanToInt.js
+++ leetcode/13_romanToInt.js
@@ -1,6 +1,6 @@
-var romanToInt = function(s) {
+var romanToInt = function (s) {
   if (!s.length) return 0;
-  
+
   const map = {
     "I": 1,
     "V": 5,
@@ -9,26 +9,25 @@ var romanToInt = function(s) {
     "C": 100,
     "D": 500,
     "M": 1000
-  }
-  
+  };
+
   let sum = 0;
   let prev = map[s[0]];
   let next = 0;
-  
+
   for (let i = 1; i < s.length; i++) {
     next = map[s[i]];
-    
-    console.log(prev, next)
+
     if (prev < next) {
       sum -= prev;
     } else {
-      sum += prev
+      sum += prev;
     }
-    
-    console.log(sum)
+
     prev = next;
   }
-  
+
+  // add back the first number
   sum += prev;
-  return sum
+  return sum;
 };
