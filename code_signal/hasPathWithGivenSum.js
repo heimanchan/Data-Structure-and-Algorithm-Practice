4f1a5a661dc6eab7ee34823f98ca277ce965a6a4
Heiman Chan <heimanchan0@gmail.com>
2019-07-17 22:47:35 -0700

    Solve hasPathWithGivenSum

---
+++ code_signal/hasPathWithGivenSum.js
@@ -0,0 +1,12 @@
+function hasPathWithGivenSum(t, s) {
+  if (!t) return s === 0;
+  
+  return helper(t, s)
+}
+
+function helper(node, sum) {
+  if (!node.left && !node.right && node.value === sum) return true;
+  
+  sum -= node.value;
+  return (!!node.left && helper(node.left, sum)) || (!!node.right && helper(node.right, sum))
+}
