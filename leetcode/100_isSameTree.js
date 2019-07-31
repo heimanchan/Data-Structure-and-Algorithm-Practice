f48ae8effea53009737b1d03a4eab697b9b6b4c1
Heiman Chan <heimanchan0@gmail.com>
2019-07-30 20:05:44 -0700

    Solve 100. Same Tree recursively

--- leetcode/100_isSameTree.js
+++ leetcode/100_isSameTree.js
@@ -1,15 +1,22 @@
 var isSameTree = function(p, q) {
-  let queue = [p, q];
+  // let queue = [p, q];
   
-  while (queue.length) {
-    let left = queue.shift();
-    let right = queue.shift();
+  // while (queue.length) {
+  //   let left = queue.shift();
+  //   let right = queue.shift();
     
-    if (!left && !right) continue;
-    if (!left || !right || left.val !== right.val) return false;
+  //   if (!left && !right) continue;
+  //   if (!left || !right || left.val !== right.val) return false;
     
-    queue.push(left.left, right.left, left.right, right.right);
-  }
+  //   queue.push(left.left, right.left, left.right, right.right);
+  // }
   
-  return true;
+  // return true;
+
+  // Recursion
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
