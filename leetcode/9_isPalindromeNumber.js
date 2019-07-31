fb1f6cb2f2bd44387e49ff181e8fce4cb7b27dd5
Heiman Chan <heimanchan0@gmail.com>
2019-07-30 19:33:52 -0700

    Solve 9. Palindrome Number without converting input to string

--- leetcode/9_isPalindromeNumber.js
+++ leetcode/9_isPalindromeNumber.js
@@ -1,16 +1,29 @@
 var isPalindrome = function(x) {
-  let num = x.toString();
-  let p = 0;
-  let q = num.length - 1;
+  // let num = x.toString();
+  // let p = 0;
+  // let q = num.length - 1;
   
-  while (p < q) {
-    if (num[p] !== num[q]) {
-      return false;
-    }
+  // while (p < q) {
+  //   if (num[p] !== num[q]) {
+  //     return false;
+  //   }
     
-    p++;
-    q--;
-  }
+  //   p++;
+  //   q--;
+  // }
   
-  return true;
+  // return true;
+
+  // No converting to string solution
+  var isPalindrome = function (x) {
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let revertNum = 0;
+    while (x > revertNum) {
+      revertNum = revertNum * 10 + x % 10;
+      x = Math.floor(x / 10);
+    }
+
+    return x === revertNum || x === Math.floor(revertNum / 10)
+  };
 };
