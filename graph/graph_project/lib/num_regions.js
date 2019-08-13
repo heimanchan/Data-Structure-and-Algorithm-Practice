40d559875faf9884ea02523fc579955022522c62
Heiman Chan <heimanchan0@gmail.com>
2019-08-12 23:44:10 -0700

    Solve number of regions

--- graph/graph_project/lib/num_regions.js
+++ graph/graph_project/lib/num_regions.js
@@ -1,5 +1,29 @@
 function numRegions(graph) {
+  let visited = new Set;
+  let count = 0;
 
+  for (let node in graph) {
+    count += helper(node, graph, visited);
+  }
+
+  return count
+}
+
+function helper(node, graph, visited) {
+  if (visited.has(node)) return 0;
+
+  let count = 0;
+  let stack = [node];
+
+  while (stack.length) {
+    let curr = stack.pop();
+    if (visited.has(curr)) continue;
+
+    visited.add(curr);
+    stack.push(...graph[curr]);
+  }
+
+  return count + 1;
 }
 
 module.exports = {
