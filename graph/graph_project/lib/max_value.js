e8e41b5a0ec9411d219fab002b516a698b4c3bd2
Heiman Chan <heimanchan0@gmail.com>
2019-08-12 23:04:57 -0700

    Solve maxValue

--- graph/graph_project/lib/max_value.js
+++ graph/graph_project/lib/max_value.js
@@ -1,5 +1,17 @@
 function maxValue(node, visited=new Set()) {
+  let stack = [node];
+  let max = -Infinity;
 
+  while (stack.length) {
+    let node = stack.pop();
+    if (visited.has(node.val)) continue;
+
+    max = Math.max(max, node.val);
+    stack.push(...node.neighbors);
+    visited.add(node.val);
+  }
+
+  return max;
 }
 
 module.exports = {
