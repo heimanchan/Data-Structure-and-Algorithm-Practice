68f62f31ad92fbbb3a2626ec6e4e34fdfca37c87
Heiman Chan <heimanchan0@gmail.com>
2019-08-16 23:51:40 -0700

    Revise 658_findClosestElements

--- leetcode/658_findClosestElements.js
+++ leetcode/658_findClosestElements.js
@@ -1,9 +1,15 @@
-var findClosestElements = function(arr, k, x) {
-  let left = 0; right = arr.length - 1;
-  
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let left = 0, right = arr.length - 1;
+
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-    
+
     if (arr[mid] === x) {
       right = mid;
       break;
@@ -13,10 +19,10 @@ var findClosestElements = function(arr, k, x) {
       left = mid + 1;
     }
   }
-  
+
   left = right;
   right++;
-  
+
   while (k-- > 0) {
     if (right >= arr.length || (left >= 0 && (x - arr[left] <= arr[right] - x))) {
       left--;
@@ -24,11 +30,11 @@ var findClosestElements = function(arr, k, x) {
       right++;
     }
   }
-  
+
   let result = [];
   for (let i = left + 1; i < right; i++) {
     result.push(arr[i])
   }
-  
+
   return result;
 };
