08a2d78328c7d43ca5a0685580e98460b9afca3c
Heiman Chan <heimanchan0@gmail.com>
2019-08-26 22:01:36 -0700

    Revise 700. Search in a Binary Search Tree

--- leetcode/700_searchBST.js
+++ leetcode/700_searchBST.js
@@ -1,10 +1,12 @@
-var searchBST = function(root, val) {
+var searchBST = function (root, val) {
   if (!root) return null;
-  if (root.val === val) return root;
-  
-  if (root.val > val) {
-    return searchBST(root.left, val);
-  } else {
+
+  if (root.val === val) {
+    return root;
+  } else if (root.val < val) {
     return searchBST(root.right, val);
+  } else if (root.val > val) {
+    return searchBST(root.left, val)
   }
+
 };
