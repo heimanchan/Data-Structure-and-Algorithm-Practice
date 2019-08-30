cbd8ca169078bbf3f57a739cd1b5e27ffef0f619
Heiman Chan <heimanchan0@gmail.com>
2019-08-30 00:45:19 -0700

    Solve 448. Find All Numbers Disappeared in an Array

---
+++ leetcode/448_findDisappearedNumbers.js
@@ -0,0 +1,17 @@
+var findDisappearedNumbers = function(nums) {
+  let result = [];
+  
+  for (let i = 0; i < nums.length; i++) {
+    let idx = Math.abs(nums[i]) - 1;
+    
+    if (nums[idx] > 0) {
+      nums[idx] = -nums[idx];
+    }
+  }
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) result.push(i + 1)
+  }
+  
+  return result;
+};
