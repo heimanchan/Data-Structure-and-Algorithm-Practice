1f46d50587879bf9fa1bb201be45e159034135ab
Heiman Chan <heimanchan0@gmail.com>
2019-08-30 22:59:29 -0700

    Solve 169. Majority Element

---
+++ leetcode/169_majorityElement.js
@@ -0,0 +1,11 @@
+var majorityElement = function(nums) {
+  let counts = {};
+  let majority = Math.floor(nums.length / 2);
+  
+  for (let num of nums) {
+    counts[num] = (counts[num] || 0) + 1;
+    
+    if (counts[num] > majority) return num;
+  }
+  
+};
