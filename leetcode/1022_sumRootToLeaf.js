36ec7ed4762c5dd524f59709aae6184230cb1cc0
Heiman Chan <heimanchan0@gmail.com>
2019-09-26 14:03:15 -0700

    Revise 1022. Sum of Root To Leaf Binary Numbers

--- leetcode/1022_sumRootToLeaf.js
+++ leetcode/1022_sumRootToLeaf.js
@@ -1,16 +1,18 @@
-var sumRootToLeaf = function(root) {
+var sumRootToLeaf = function (root) {
   let result = 0;
-  dfs(root, '');
+  dfs(root, "");
   return result;
 
-  function dfs(root, binaryStr) {
-      if(!root) return;
-      binaryStr += root.val;
-      if(!root.left && !root.right) {
-          result += parseInt(binaryStr, 2);
-          return;
-      }
-      dfs(root.left, binaryStr);
-      dfs(root.right, binaryStr);
+  function dfs(node, binaryStr) {
+    if (!node) return;
+    binaryStr += node.val;
+
+    if (!node.left && !node.right) {
+      result += parseInt(binaryStr, 2);
+      return;
+    }
+
+    dfs(node.left, binaryStr);
+    dfs(node.right, binaryStr);
   }
 };
