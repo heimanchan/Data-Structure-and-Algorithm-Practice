25d9c8b29e0784337663e73b906f1d190842c2d9
Heiman Chan <heimanchan0@gmail.com>
2019-10-03 01:42:19 -0700

    Solve 884. Uncommon Words from Two Sentences

--- Hackerrank/sort_the_events.js
+++ Hackerrank/sort_the_events.js
@@ -1,71 +1,81 @@
 function getEventsOrder(team1, team2, events1, events2) {
-    // Write your code here
-    let reg = /\d+\+?\d?/;
+  // Write your code here
+  let reg = /\d+\+?\d?/;
 
-    let firstHalfMap = {};
-    let secondHalfMap = {};
+  let firstHalfMap = {};
+  let secondHalfMap = {};
 
-    let firstHalf = [];
-    let secondHalf = [];
+  let firstHalf = [];
+  let secondHalf = [];
 
-    let result = [];
+  let result = [];
 
-    for (let i = 0; i < events1.length; i++) {
-        let findNum = events1[i].match(reg);
+  for (let i = 0; i < events1.length; i++) {
+    let findNum = events1[i].match(reg);
 
-        if (parseInt(findNum[0]) <= 45) {
-            firstHalf.push(`${team1} ${events1[i]}`)
-        } else {
-            secondHalf.push(`${team1} ${events1[i]}`)
-        }
+    if (parseInt(findNum[0]) <= 45) {
+      firstHalf.push(`${team1} ${events1[i]}`)
+    } else {
+      secondHalf.push(`${team1} ${events1[i]}`)
+    }
+  }
+
+  for (let i = 0; i < events2.length; i++) {
+    let findNum = events2[i].match(reg);
+
+    if (parseInt(findNum[0]) <= 45) {
+      firstHalf.push(`${team2} ${events2[i]}`)
+    } else {
+      secondHalf.push(`${team2} ${events2[i]}`)
+    }
+  }
+
+  for (let i = 0; i < firstHalf.length; i++) {
+    let time;
+    let findNum = firstHalf[i].match(reg);
+
+    if (findNum[0].match(/\+/)) {
+      let mins = findNum[0].split("+");
+      time = parseInt(mins[0]) + parseInt(mins[1])
+    } else {
+      console.log(findNum[0])
+      time = parseInt(findNum[0])
     }
 
-    for (let i = 0; i < events2.length; i++) {
-        let findNum = events2[i].match(reg);
+    if (firstHalfMap[time]) {
+      firstHalfMap[time].push(firstHalf[i]);
+    } else {
+      firstHalfMap[time] = [firstHalf[i]];
+    }
+  }
 
-        if (parseInt(findNum[0]) <= 45) {
-            firstHalf.push(`${team2} ${events2[i]}`)
-        } else {
-            secondHalf.push(`${team2} ${events2[i]}`)
-        }
+  for (let i = 0; i < secondHalf.length; i++) {
+    let time;
+    let findNum = secondHalf[i].match(reg);
+
+    if (findNum[0].match(/\+/)) {
+      let mins = findNum[0].split("+");
+      time = parseInt(mins[0]) + parseInt(mins[1])
+    } else {
+      time = parseInt(findNum[0])
     }
 
-    for (let i = 0; i < firstHalf.length; i++) {
-        let time;
-        let findNum = firstHalf[i].match(reg);
-
-        if (findNum[0].match(/\+/)) {
-            let mins = findNum[0].split("+");
-            time = parseInt(mins[0]) + parseInt(mins[1])
-            firstHalfMap[time] = firstHalf[i];
-        } else {
-            time = parseInt(findNum[0])
-            firstHalfMap[time] = firstHalf[i];
-        }
+    if (secondHalfMap[time]) {
+      secondHalfMap[time].push(secondHalf[i]);
+    } else {
+      secondHalfMap[time] = [secondHalf[i]];
     }
+  }
 
-    for (let i = 0; i < secondHalf.length; i++) {
-        let time;
-        let findNum = secondHalf[i].match(reg);
+  for (let time in firstHalfMap) {
+    firstHalfMap[time].sort();
+    result.push(...firstHalfMap[time])
+  }
 
-        if (findNum[0].match(/\+/)) {
-            let mins = findNum[0].split("+");
-            time = parseInt(mins[0]) + parseInt(mins[1])
-            secondHalfMap[time] = secondHalf[i];
-        } else {
-            time = parseInt(findNum[0])
-            secondHalfMap[time] = secondHalf[i];
-        }
-    }
+  for (let time in secondHalfMap) {
+    secondHalfMap[time].sort();
+    result.push(...secondHalfMap[time])
+  }
 
-    for (let time in firstHalfMap) {
-        result.push(firstHalfMap[time])
-    }
-
-    for (let time in secondHalfMap) {
-        result.push(secondHalfMap[time])
-    }
-
-    // console.log(firstHalfMap, secondHalfMap)
-    return result
+  return result
 }
--- leetcode/884_uncommonFromSentences.js
+++ leetcode/884_uncommonFromSentences.js
@@ -1,11 +1,10 @@
 var uncommonFromSentences = function(A, B) {
-    let words = A.split(" ").concat(B.split(" "));
-    let counts = {};
-    for(let i = 0; i < words.length; i++){
-        if(counts.hasOwnProperty(words[i]) === false)
-            counts[words[i]] = 0; 
-        counts[words[i]]++;
-    }
-    
-    return words.filter(word => counts[word] === 1);
+  let words = A.split(" ").concat(B.split(" "));
+  let counts = {};
+
+  for (let word of words) {
+    counts[word] = (counts[word] || 0) + 1;
+  }
+
+  return words.filter(word => counts[word] === 1);
 };
