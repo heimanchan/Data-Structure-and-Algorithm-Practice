f802e02998ec5397fba2497d666581847327db94
Heiman Chan <heimanchan0@gmail.com>
2019-10-10 17:21:11 -0700

    Solve 985. Sum of Even Numbers After Queries

--- leetcode/985_sumEvenAfterQueries.js
+++ leetcode/985_sumEvenAfterQueries.js
@@ -1,20 +1,23 @@
-var sumEvenAfterQueries = function(A, queries) {
+var sumEvenAfterQueries = function (A, queries) {
+  let sum = 0;
   let result = [];
-  
+
+  A.forEach(num => num % 2 === 0 ? sum += num : 0);
+
   for (let i = 0; i < queries.length; i++) {
-    let query = queries[i];
-    
-    A[query[1]] += query[0];
-    result.push(sumEven(A))
+    let val = queries[i][0];
+    let idx = queries[i][1];
+
+    if (isEven(A[idx])) sum -= A[idx];
+    A[idx] += val;
+    if (isEven(A[idx])) sum += A[idx];
+
+    result.push(sum);
   }
-  
+
   return result;
-  
-  function sumEven(arr) {
-    let sum = 0;
-    
-    arr.forEach(num => num % 2 === 0 ? sum += num : 0);
-    
-    return sum;
+
+  function isEven(num) {
+    return num % 2 === 0;
   }
 };
