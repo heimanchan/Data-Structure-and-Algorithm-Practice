bbe089836168e8d69fcaf1864639f0316515f334
Heiman Chan <heimanchan0@gmail.com>
2022-09-02 23:15:45 -0700

    Solve decodeString

--- string/decodeString.js
+++ string/decodeString.js
@@ -1,4 +1,4 @@
-function decodeString(s) {
+const decodeString = (s) => {
   let k = "";
   const countStack = [];
   const strStack = [];
@@ -6,38 +6,72 @@ function decodeString(s) {
 
   for (let i = s.length - 1; i >= 0; i--) {
     if (s[i] === "{" || s[i] === "}") {
-      console.log(s[i], "continue");
       continue;
-    } else if (s[i] % 10 == s[i]) {
-      console.log(s[i], "k");
-      // k = k * 10 + parseInt(s[i]);
+    } else if (!isNaN(s[i])) {
       k = s[i] + k;
     } else if (s[i] === ")") {
-      console.log(s[i], ")");
       countStack.push(k);
-      strStack.push(currentStr);
       k = "";
+      strStack.push(currentStr);
       currentStr = "";
-      console.log("countStack", countStack);
+      console.log(countStack, strStack);
     } else if (s[i] === "(") {
-      console.log(s[i], "(");
       let decodeStr = "";
       let count = countStack.pop();
-      for (let j = 0; j < count; j++) {
-        console.log("add decodeStr", j, count);
+
+      for (let i = 0; i < count; i++) {
         decodeStr += currentStr;
       }
 
       currentStr = decodeStr;
-      console.log(s[i], currentStr);
     } else {
       currentStr = s[i] + currentStr;
-      console.log(s[i], currentStr);
     }
   }
 
   return currentStr;
-}
+};
 
 console.log(decodeString("(ab(d){3}){2}"));
 console.log(decodeString("(ab(d){10}){3}"));
+
+// function decodeString(s) {
+//   let k = "";
+//   const countStack = [];
+//   const strStack = [];
+//   let currentStr = "";
+
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     if (s[i] === "{" || s[i] === "}") {
+//       console.log(s[i], "continue");
+//       continue;
+//     } else if (s[i] % 10 == s[i]) {
+//       console.log(s[i], "k");
+//       // k = k * 10 + parseInt(s[i]);
+//       k = s[i] + k;
+//     } else if (s[i] === ")") {
+//       console.log(s[i], ")");
+//       countStack.push(k);
+//       strStack.push(currentStr);
+//       k = "";
+//       currentStr = "";
+//       console.log("countStack", countStack);
+//     } else if (s[i] === "(") {
+//       console.log(s[i], "(");
+//       let decodeStr = "";
+//       let count = countStack.pop();
+//       for (let j = 0; j < count; j++) {
+//         console.log("add decodeStr", j, count);
+//         decodeStr += currentStr;
+//       }
+
+//       currentStr = decodeStr;
+//       console.log(s[i], currentStr);
+//     } else {
+//       currentStr = s[i] + currentStr;
+//       console.log(s[i], currentStr);
+//     }
+//   }
+
+//   return currentStr;
+// }
